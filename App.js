99642630e1d6ba312b3c364c07fe0b7650606c43
jicjjang <jicjjang12@gmail.com>
2016-04-12 21:57:14 +0900

    3. the-render-method

--- App.js
+++ App.js
@@ -2,21 +2,14 @@ import React from 'react';
 
 class App extends React.Component {
 	render() {
-		return <div>Hello!</div>
+		return (
+			<div>
+				<h1>Hello world</h1>
+				<b>Bold</b>
+			</div>
+		)
 	}
 }
-
-// or
-
-// class App extends React.Component {
-// 	render () {
-// 		return React.createElement('h1', null, 'Hello guys!!')
-// 	}
-// }
-
-// or
-
-// const App = () => <h1>Hello Eggheads</h1>
 
 export default App
 
