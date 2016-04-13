9cb04bcaba12b482731102208d47bef39202120b
jicjjang <jicjjang12@gmail.com>
2016-04-13 18:00:09 +0900

    12. react-fundamentals-higher-order-components-replaces-mixins

--- App.js
+++ App.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+let Mixin = InnerComponent => class extends React.Component {
 	constructor() {
 		super()
 		this.update = this.update.bind(this)
-		this.state = {increasing: false}
+		this.state = {val: 0}
 	}
 	update() {
-		ReactDOM.render(
-			<App val={this.props.val+1} />,
-			document.getElementById('app')
-		)
+		this.setState({val: this.state.val + 1})
 	}
-	// 최초엔 호출 x. 컴포넌트 갱신되기 전 호출
-	componentWillReceiveProps(nextProps) {
-		this.setState({increasing: nextProps.val
-			> this.props.val})
-	}
-	// true/false를 리턴해야 함.
-	// render 전에 호출 되며, false이면
-	// render를 하지 않음.
-	shouldComponentUpdate(nextProps, nextState) {
-		return nextProps.val % 5 === 0;
+	componentWillMount() {
+		console.log('will mount')
 	}
 	render() {
-		console.log(this.state.increasing)
-		return (
-			<button onClick={this.update}>
-				{this.props.val}
-			</button>
-		)
+		return <InnerComponent
+					update={this.update}
+					{...this.state}
+					{...this.props} />
 	}
-	// 최초엔 호출 x. 컴포넌트 갱신 후 호출
-	componentDidUpdate(prevProps, prevState) {
-		console.log('prevProps', prevProps);
+	componentDidMount() {
+		console.log('mounted')
 	}
 }
 
-App.defaultProps = { val: 0 }
+const Button = (props) => <button
+							onClick={props.update}>
+							{props.txt} - {props.val}
+						</button>
+
+const Label = (props) => <label
+							onMouseMove={props.update}>
+							{props.txt} - {props.val}
+						</label>
+
+let ButtonMixed = Mixin(Button)
+let LabelMixed = Mixin(Label)
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<ButtonMixed txt="Button" />
+				<LabelMixed txt="Label" />
+			</div>
+		)
+	}
+}
 
 export default App
